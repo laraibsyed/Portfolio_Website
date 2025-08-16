258d291d2153cf51b1d5ab0042bb2b67ec06f0d5
Laraib Syed <92629797+laraibsyed@users.noreply.github.com>
2025-08-16 23:27:28 +0400

    nav bar done

---
+++ index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const words = ["Data Scientist", "Machine Learning Engineer", "AI Engineer"];
+    let i = 0;
+    let j = 0;
+    let isDeleting = false;
+    let speed = 150;
+
+    function typingEffect() {
+        const jobElement = document.getElementById("job");
+        const currentWord = words[i];
+        
+        if (!isDeleting) {
+            jobElement.textContent = currentWord.substring(0, j + 1);
+            j++;
+            if (j === currentWord.length) {
+                isDeleting = true;
+                speed = 1000;
+            }
+        } else {
+            jobElement.textContent = currentWord.substring(0, j - 1);
+            j--;
+            if (j === 0) {
+                isDeleting = false;
+                i = (i + 1) % words.length;
+            }
+        }
+
+        setTimeout(typingEffect, speed);
+        speed = isDeleting ? 100 : 150;
+    }
+
+    typingEffect();
+});
