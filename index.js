c6e621fd2f416a4f5afa73ac7435c342a8c840c0
Laraib Syed <92629797+laraibsyed@users.noreply.github.com>
2025-08-21 16:20:22 +0400

    about me done yay

--- index.js
+++ index.js
@@ -31,3 +31,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     typingEffect();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const words = ["Working", "Drinking Matcha", "Watching F1","Coding", "Building Legos", "Developing"];
+    let i = 0;
+    let j = 0;
+    let isDeleting = false;
+    let speed = 150;
+
+    function typingEffect() {
+        const jobElement = document.getElementById("statusinfo");
+        const currentWord = words[i];
+        
+        if (!isDeleting) {
+            jobElement.textContent = currentWord.substring(0, j + 1);
+            j++;
+            if (j === currentWord.length) {
+                isDeleting = true;
+                speed = 1000;
+            }
+        } else {
+            jobElement.textContent = currentWord.substring(0, j - 1);
+            j--;
+            if (j === 0) {
+                isDeleting = false;
+                i = (i + 1) % words.length;
+            }
+        }
+
+        setTimeout(typingEffect, speed);
+        speed = isDeleting ? 100 : 150;
+    }
+
+    typingEffect();
+});
